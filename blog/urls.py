9d71a501433c9011f46615470e8d514bbe64cc2a
Sarojabhandari <sarojabhandari4@gmail.com>
2021-05-30 18:41:57 +0545

    django template formatting, view for post lists

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
 
-from .views import index
+from .views import index, post_list
+
 
 urlpatterns = [
     path('', index, name='index'),
+   path('post-list/', post_list, name='post-list'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,14 @@ from blog.models import Post
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse\
 
 
 def index(request):
-    posts= Post.objects.all()
+    posts= Post.objects.all().order_by("-id")[:3]
     return render(request, 'blog/index.html', {'posts': posts})
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/blog-list.html', {'posts': posts})
 
